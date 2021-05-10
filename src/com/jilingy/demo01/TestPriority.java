03a35a7f3256d06f30e40e535f7421babd1038fd
Jiling Yang <jilingy@foxmail.com>
2021-05-10 10:16:56 +0800

    jilingy add state，priority 2021-05-10

---
+++ src/com/jilingy/demo01/TestPriority.java
@@ -0,0 +1,10 @@
+package com.jilingy.demo01;
+
+/**
+ * @Description:
+ * @Author: yangjiling
+ * @Email: jilingy@foxmail.com
+ * @Date: 2021/5/10
+ **/
+public class TestPriority {
+}
---
+++ src/com/jilingy/demo01/TestState.java
@@ -0,0 +1,38 @@
+package com.jilingy.demo01;
+
+/**
+ * @Description:观察测试线程的状态
+ * @Author: yangjiling
+ * @Email: jilingy@foxmail.com
+ * @Date: 2021/5/8
+ **/
+public class TestState {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread=new Thread(()->{
+        for (int i = 0; i <5 ; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("///////");
+        }
+        });
+
+        //观测状态
+
+        Thread.State state=thread.getState();
+        System.out.println(state);//NEW
+
+        //观察启动后
+        thread.start();
+        state=thread.getState();
+        System.out.println(state);//RUNNABLE
+        while (state!=Thread.State.TERMINATED){
+            Thread.sleep(100);
+            state=thread.getState();
+            System.out.println(thread.getState());
+        }
+        //死亡后的线程不能再启动
+    }
+}
